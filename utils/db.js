4d17515770c9a08b5dbf1199b4e968c892e04869
Ankur Saxena <ankur.rmp2002@gmail.com>
2024-12-04 19:18:48 +0530

    see

--- utils/db.js
+++ utils/db.js
@@ -1,32 +1,13 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const { Pool } = require('pg');
 
-const pool = mysql.createPool({
+const pool = new Pool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  port: process.env.DB_PORT || 3306,
 });
 
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Failed to connect to the MySQL server:', err.message);
-    if (err.code === 'ER_ACCESS_DENIED_ERROR') {
-      console.error('Check your MySQL username and password.');
-    } else if (err.code === 'ENOTFOUND') {
-      console.error('Database host not found. Verify DB_HOST in .env.');
-    } else if (err.code === 'ECONNREFUSED') {
-      console.error('Connection refused. Ensure MySQL server is running.');
-    }
-  } else {
-    console.log('Successfully connected to the MySQL server!');
-    connection.release();
-  }
-});
-
-module.exports = pool.promise(); 
+pool.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Database connection error:', err));
